e6f5761c4d552fa7164c86da8d03bcd2b34ff785
hisui <henryharlay@gmail.com>
2026-02-04 00:50:47 +0900

    add keymaps

--- init.lua
+++ init.lua
@@ -4,5 +4,6 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
 require("config.lazy")
+require("config.keymaps")
 
 vim.g.mapleader = " "
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,16 @@
+--   ___ ___ .__             .__/\             .__                        --
+--  /   |   \|__| ________ __|__)/______ ___  _|__| ____________   ____   --
+-- /    ~    \  |/  ___/  |  \  |/  ___/ \  \/ /  |/     \_  __ \_/ ___\  --
+-- \    Y    /  |\___ \|  |  /  |\___ \   \   /|  |  Y Y  \  | \/\  \___  --
+--  \___|_  /|__/____  >____/|__/____  >   \_/ |__|__|_|  /__|    \___  > --
+--        \/         \/              \/                 \/            \/  --
+
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- vim.o.number = true
+vim.o.autoindent = true
+vim.o.tabstop = 4
+vim.api.nvim_set_keymap("i", "jj", "<ESC>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Space><Space>", ":wq<CR>", { noremap = true, silent = true })
