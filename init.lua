2ec8d8e20613edbb97b3a78b66d03c36318030f5
hisui <henryharlay@gmail.com>
2026-02-04 02:11:33 +0900

    base settings nerdfonts, scroll etc

--- init.lua
+++ init.lua
@@ -9,3 +9,9 @@ require("config.keymaps")
 vim.g.mapleader = " "
 vim.g.have_nerd_font = true
 vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+
+-- Line wrapping on/off
+-- vim.opt.wrap = false         -- wrap line
+-- vim.opt.sidescroll = 8       -- scroll step
+-- vim.opt.sidescrolloff = 12   -- Minimum columns to keep to the left/right
