d978279fef01698b9b885f021f5dea67767fd7c7
hisui <henryharlay@gmail.com>
2026-02-04 12:10:49 +0900

    add maplocalleader

--- init.lua
+++ init.lua
@@ -7,6 +7,7 @@ require("config.lazy")
 require("config.keymaps")
 
 vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 vim.g.have_nerd_font = true
 vim.opt.termguicolors = true
 vim.opt.signcolumn = "yes"
